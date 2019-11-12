c030714b3c254c520c11ba9e7870bd6aac2a30db
David Olivari <david.olivari@gmail.com>
2019-11-12 11:42:52 +0100

    carriage return feature

--- sketch.js
+++ sketch.js
@@ -1,73 +1,77 @@
 // noprotect
-let imgs = {} // Declare variable 'img'.
-let pos = 0
-let temperature = -55
+let imgs = {}; // Declare variable 'img'.
+let pos = 0;
+let posy = 100;
+let temperature = -55;
 
 function preload() {
   loadJSON(
-    'https://api.darksky.net/forecast/47c38c2ea901f7bdb4b163a3d56d4459/50.8549541,4.3053507?units=si',
+    "https://api.darksky.net/forecast/47c38c2ea901f7bdb4b163a3d56d4459/50.8549541,4.3053507?units=si",
     gotMeteo,
-    'jsonp'
-  )
+    "jsonp"
+  );
 }
 
 function gotMeteo(data) {
-  console.log('temperature :', data.currently.temperature)
-  temperature = data.currently.temperature
-  setTimeout(preload, 60000)
+  console.log("temperature :", data.currently.temperature);
+  temperature = data.currently.temperature;
+  setTimeout(preload, 60000);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  for (let folder of ['cold', 'coold', 'cooold', 'hot', 'hoot', 'hooot']) {
-    imgs[folder] = {}
+  createCanvas(windowWidth, windowHeight);
+  for (let folder of ["cold", "coold", "cooold", "hot", "hoot", "hooot"]) {
+    imgs[folder] = {};
     for (let i = 0; i < 26; i++) {
-      let char = (10 + i).toString(36)
-      let filename = 'img/' + folder + '/' + char + '.png'
+      let char = (10 + i).toString(36);
+      let filename = "img/" + folder + "/" + char + ".png";
       imgs[folder][char] = loadImage(
         filename,
         () => {
-          console.log(`${filename} loaded`)
+          console.log(`${filename} loaded`);
         },
         () => {
-          console.error(`${filename} missing`)
+          console.error(`${filename} missing`);
         }
-      )
+      );
     }
   }
 }
 
-function draw() {}
-
 function keyPressed() {
-  text('temperature : ' + temperature + ' °C', 10, 300)
-  if (' ' === key) {
-    pos += 100
+  fill(255);
+  text("temperature : " + temperature + " °C", 10, 20);
+  if (" " === key) {
+    pos += 100;
+  } else if ("Enter" === key) {
+    posy += 100;
+    pos = 0;
   } else {
     try {
       if (temperature < 0) {
-        image(imgs.cooold[key], pos, windowHeight / 2)
-        pos += imgs.cooold[key].width
+        image(imgs.cooold[key], pos, posy);
+        pos += imgs.cooold[key].width;
       } else if (temperature < 5) {
-        image(imgs.coold[key], pos, windowHeight / 2)
-        pos += imgs.coold[key].width
+        image(imgs.coold[key], pos, posy);
+        pos += imgs.coold[key].width;
       } else if (temperature < 10) {
-        image(imgs.cold[key], pos, windowHeight / 2)
-        pos += imgs.cold[key].width
+        image(imgs.cold[key], pos, posy);
+        pos += imgs.cold[key].width;
       } else if (temperature < 15) {
-        image(imgs.hot[key], pos, windowHeight / 2)
-        pos += imgs.hot[key].width
+        image(imgs.hot[key], pos, posy);
+        pos += imgs.hot[key].width;
       } else if (temperature < 20) {
-        image(imgs.hoot[key], pos, windowHeight / 2)
-        pos += imgs.hoot[key].width
+        image(imgs.hoot[key], pos, posy);
+        pos += imgs.hoot[key].width;
       } else {
-        image(imgs.hooot[key], pos, windowHeight / 2)
-        pos += imgs.hooot[key].width
+        image(imgs.hooot[key], pos, posy);
+        pos += imgs.hooot[key].width;
       }
     } catch (err) {
-      console.error(err)
-      background(255)
-      pos = 0
+      console.error(err);
+      background(0);
+      pos = 0;
+      posy = 100;
     }
   }
 }
